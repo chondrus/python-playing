ad851cbb87567baceb2c8aca977adb9252a0cc33
chondrus <kelsey.m.harris.11@alum.dartmouth.org>
2011-09-17 19:38:53 -0400

    arg fooling

---
+++ command-line-args/arg-flag-level-echo.py
@@ -0,0 +1,12 @@
+# 
+# author:   chondrus
+# date:     sept 17, 2011
+# purpose:  fooling around to get a way of a level set
+#
+
+import sys
+
+if sys.argv[1] == "debug-level":
+    # set debug level
+    debug_level = sys.argv[2]
+    print debug_level
